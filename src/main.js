dae5514ef1f23213b720bacc19957edb3f0d7180
behemothoz <behemothoz@yandex.ru>
2018-06-14 16:07:24 +0300

    added rxJs

--- src/main.js
+++ src/main.js
@@ -1,22 +1,40 @@
+import { map, flatMap, filter, buffer, take, bufferCount, sequenceEqual } from 'rxjs/operators';
+import { Observable, fromEvent, pipe, of, from, merge } from 'rxjs';
+
+
 let app = document.querySelector(`#app`);
 
 // State
+/*
+  rectangles - кол-во прямоугольников на экране
+  expectedClicks - ожидаемая последовательность кликов (будет увеличиваться / будет проверятся с пользовательской последовательностью)
+  score - счет (увеличиваетсяна единицу)
+  started - начало игры (true - старт игры)
+  ended - конец игры (true - конец игры)
+
+  TODO - окончание игры как win
+*/
 let initialState = {
-  level: 0,
-  pattern: 3,
-  start: true,
-  end: false
+  rectangles: [`orange`, `blue`, `red`, `green`],
+  expectedClicks: [`orange`, `blue`, `red`, `green`],
+  score: 0,
+  started: false,
+  ended: false
 }
 
-let rectangles = [`orange`, `blue`, `red`, `green`]; // 4 rectangels
 
-let headerTemplate = (state) => {
+
+/*
+  Render screens
+*/
+
+let renderHeaderTemplate = (state) => {
   return `<div class="header">
-            <div class="level">Level: <span>${state.level}</span></div>
+            <div class="level">Score: <span>${state.score}</span></div>
           </div>`;
 }
 
-let startScreen = () => {
+let renderStartScreen = () => {
   return `<div class="screen-start">
             <h1 class="title">Puzzle</h1>
             <p class="description"><b>Rules.</b> Four colored buttons light up in a certain order. After the drawing is displayed, you need to repeat the template by clicking the buttons in the correct order. The template becomes longer with each successful attempt. The error leads to the end of the game.</p>
@@ -24,27 +42,98 @@ let startScreen = () => {
           </div>`;
 }
 
-let gameScreen = (state) => {
-  return `${headerTemplate(state)}
+let renderGameScreen = (state) => {
+  return `${renderHeaderTemplate(state)}
           <div class="screen-rectangles">
-            ${rectangles.map(color => `<div class="rectangle ${color}"></div>`).join('')}
+            ${state.rectangles.map(color => `<div class="rectangle ${color}" data-color="${color}"></div>`).join('')}
           </div>`;
 }
 
-let endScreen = () => {
+let renderEndScreen = () => {
   return `<div class="screen-end">
             <h1 class="title">Game over</h1>
           </div>`;
 }
 
-let toggleScreen = (state) => {
+let renderSelectedScreen = (state) => {
   return `<div class="main">
-            ${(!state.start && !state.end) ? startScreen() : (state.start && !state.end) ? gameScreen(state) : endScreen()}
+            ${(!state.started && !state.ended) ? renderStartScreen() : (state.started && !state.ended) ? renderGameScreen(state) : renderEndScreen()}
           </div>`;
 }
 
-let renderScreen = (state) => {
-  app.innerHTML = toggleScreen(state);
+let renderToDom = (state) => {
+  app.innerHTML = renderSelectedScreen(state);
+  initGame(state);
 }
 
-renderScreen(initialState);
+renderToDom(initialState);
+
+
+
+/*
+  Logics app
+*/
+
+function initGame(state) {
+
+  // Вспомогательная функци: фильтрация кликов (только на прямоугольниках)
+  let filterRectangle = (target) => {
+    return target == `orange` || target == `blue` || target == `red` || target == `green`;
+  }
+
+  // Stream clicks on document
+  let clickDocument$ = fromEvent(document, `click`);
+
+  // Stream click button start
+  let clickButtonStart$ = clickDocument$.pipe(
+      filter(event => event.target.classList.contains(`btn-start`)),
+      map(_ => true)
+    );
+
+  // Stream filtered clicks on document (rectangle)
+  let filteredClickRectangle$ = clickDocument$.pipe(
+      filter(event => filterRectangle(event.target.dataset.color))
+    );
+
+  // Stream selected rectangle
+  let clickRectangle$ = filteredClickRectangle$.pipe(
+      map(event => event.target),
+      map(elem => elem.dataset.color)
+    );
+
+  // Comparison with the expected sequence
+  // Uses the state: expectedClicks.length and expectedClicks
+  let sequenceComparison$ = clickRectangle$.pipe(bufferCount(state.expectedClicks.length), flatMap(sequence => {
+    return from(state.expectedClicks).pipe(sequenceEqual(from(sequence)))
+  }));
+
+
+  // Subscribe (start)
+  clickButtonStart$.subscribe(result => {
+    // Copy init state + change started
+    let newState = Object.assign({}, state, {started: true});
+
+    renderToDom(newState);
+  })
+
+  // Subscribe (game process)
+  sequenceComparison$.subscribe(result => {
+    let newState;
+    
+    if (result) {
+      newState = Object.assign({}, state, {
+        score: state.score + 1,
+        started: true
+      });
+
+      renderToDom(newState);
+    }
+
+    if (!result) {
+      newState = Object.assign({}, state, {ended: true});
+
+      renderToDom(newState);
+    }
+  })
+
+}
