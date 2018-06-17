4df8456aa86bc9dfcab5e8a14e907cdae1025bf5
behemothoz <behemothoz@yandex.ru>
2018-06-17 13:57:39 +0300

    simple refactoring

--- src/main.js
+++ src/main.js
@@ -75,16 +75,15 @@ renderToDom(initialState);
   Logics app
 */
 
-
-
-/*
-  Блоки композиции: интенты, экшены, состояние и логика рендеринга.
-*/
-
-// actions$ - зависит от intents.yourIntent$;
-// state$ - зависит от потока actions$
+let filterRectangle = (target) => {
+  return target == `orange` || target == `blue` || target == `red` || target == `green`;
+}
 
 let click$ = fromEvent(document, `click`);
+
+/*
+  Block intents
+*/
 
 let intents = {
   startGame$: click$.pipe(
@@ -97,6 +96,11 @@ let intents = {
     map(_ => true)
   )
 }
+
+
+/*
+  Action block (depends on intents)
+*/
 
 let action$ = merge(
   intents.startGame$.pipe(
@@ -112,74 +116,20 @@ let action$ = merge(
   )
 )
 
+
+/*
+  State block (depends on action)
+*/
+
 let state$ = action$.pipe(
   scan((state, func) => func(state), initialState)
 )
 
+
+/*
+  Render block
+*/
+
 state$.subscribe(state => 
   renderToDom(state)
 );
-
-
-// function initGame(state) {
-  // Вспомогательная функци: фильтрация кликов (только на прямоугольниках)
-  // let filterRectangle = (target) => {
-  //   return target == `orange` || target == `blue` || target == `red` || target == `green`;
-  // }
-
-  // Stream clicks on document
-  // let clickDocument$ = fromEvent(document, `click`);
-
-  // Stream click button start
-  // let clickButtonStart$ = clickDocument$.pipe(
-  //     filter(event => event.target.classList.contains(`btn-start`)),
-  //     map(_ => true)
-  //   );
-
-  // Stream filtered clicks on document (rectangle)
-  // let filteredClickRectangle$ = clickDocument$.pipe(
-  //     filter(event => filterRectangle(event.target.dataset.color))
-  //   );
-
-  // Stream selected rectangle
-  // let clickRectangle$ = filteredClickRectangle$.pipe(
-  //     map(event => event.target),
-  //     map(elem => elem.dataset.color)
-  //   );
-
-  // Comparison with the expected sequence
-  // Uses the state: expectedClicks.length and expectedClicks
-  // let sequenceComparison$ = clickRectangle$.pipe(bufferCount(state.expectedClicks.length), flatMap(sequence => {
-  //   return from(state.expectedClicks).pipe(sequenceEqual(from(sequence)))
-  // }));
-
-
-  // Subscribe (start)
-  // clickButtonStart$.subscribe(result => {
-  //   // Copy init state + change started
-  //   let newState = Object.assign({}, state, {started: true});
-
-  //   renderToDom(newState);
-  // })
-
-  // Subscribe (game process)
-  // sequenceComparison$.subscribe(result => {
-  //   let newState;
-    
-  //   if (result) {
-  //     newState = Object.assign({}, state, {
-  //       score: state.score + 1,
-  //       started: true
-  //     });
-
-  //     renderToDom(newState);
-  //   }
-
-  //   if (!result) {
-  //     newState = Object.assign({}, state, {ended: true});
-
-  //     renderToDom(newState);
-  //   }
-  // })
-
-// }
---
+++ src/main_old.js
@@ -0,0 +1,62 @@
+function initGame(state) {
+  // Вспомогательная функция: фильтрация кликов (только на прямоугольниках)
+  let filterRectangle = (target) => {
+    return target == `orange` || target == `blue` || target == `red` || target == `green`;
+  }
+
+  // Stream clicks on document
+  let clickDocument$ = fromEvent(document, `click`);
+
+  // Stream click button start
+  let clickButtonStart$ = clickDocument$.pipe(
+      filter(event => event.target.classList.contains(`btn-start`)),
+      map(_ => true)
+    );
+
+  // Stream filtered clicks on document (rectangle)
+  let filteredClickRectangle$ = clickDocument$.pipe(
+      filter(event => filterRectangle(event.target.dataset.color))
+    );
+
+  // Stream selected rectangle
+  let clickRectangle$ = filteredClickRectangle$.pipe(
+      map(event => event.target),
+      map(elem => elem.dataset.color)
+    );
+
+  // Comparison with the expected sequence
+  // Uses the state: expectedClicks.length and expectedClicks
+  let sequenceComparison$ = clickRectangle$.pipe(bufferCount(state.expectedClicks.length), flatMap(sequence => {
+    return from(state.expectedClicks).pipe(sequenceEqual(from(sequence)))
+  }));
+
+
+  // Subscribe (start)
+  clickButtonStart$.subscribe(result => {
+    // Copy init state + change started
+    let newState = Object.assign({}, state, {started: true});
+
+    renderToDom(newState);
+  })
+
+  // Subscribe (game process)
+  sequenceComparison$.subscribe(result => {
+    let newState;
+    
+    if (result) {
+      newState = Object.assign({}, state, {
+        score: state.score + 1,
+        started: true
+      });
+
+      renderToDom(newState);
+    }
+
+    if (!result) {
+      newState = Object.assign({}, state, {ended: true});
+
+      renderToDom(newState);
+    }
+  })
+
+}
